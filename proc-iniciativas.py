8d47b213d7262af4075b7e85dd3a710cffb075c7
Frederico Muñoz <fsmunoz@gmail.com>
2023-10-06 16:53:22 +0100

    Fix GPL notice

--- proc-iniciativas.py
+++ proc-iniciativas.py
@@ -18,7 +18,7 @@
 # License for more details.
 #
 #   You should have received a copy of the GNU General Public License
-#   along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+#   along with p3. If not, see <https://www.gnu.org/licenses/>.
 
 
 ## Imports ####################################################
